3376b1c26f379d1b7fe42e95b074a7231b526632
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-06 04:48:27 +0700

    Update xx.py

--- xx.py
+++ xx.py
@@ -6,13 +6,15 @@ password = 'fb_password'
 
 url = 'http://www.simplysit.com/01496//vwIQW/?sc=1&sc=1&l=1&ppy=5772259&i=5772259'
 
-driver = webdriver.Chrome("/Users/johnfisher/Downloads/chromedriver")
+driver = webdriver.Chrome()
 
 driver.get(url)
 
-driver.find_element_by_id('email').send_keys(username)
-driver.find_element_by_id('pass').send_keys(password)
+username_box = driver.find_element_by_id('email').send_keys(username)
+username_box = driver.send_keys(usr)
+username_box = driver.find_element_by_id('pass').send_keys(password)
+username_box = driver.send_key(pwd)
 
 time.sleep(2)
 
-driver.find_element_by_id('loginbutton').click()
+login_btn = driver.find_element_by_id('loginbutton').click()
